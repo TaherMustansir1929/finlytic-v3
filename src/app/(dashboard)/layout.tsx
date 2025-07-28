6a1d6be4d49686018ac21f4ba1bc2e2fee2884bd
TaherMustansir1929 <eren.jeager1929101@gmail.com>
2025-07-28 18:36:46 +0500

    02.1: Implement Accounts GET & POST APIs

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,7 +1,13 @@
+import { currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+
 export default async function DashboardLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const user = await currentUser();
+  if (!user) redirect("/sign-in");
+
   return <div>{children}</div>;
 }
--- src/app/api/[[...route]]/accounts.ts
+++ src/app/api/[[...route]]/accounts.ts
@@ -1,5 +1,52 @@
+import { z } from "zod";
 import { Hono } from "hono";
+import { createId } from "@paralleldrive/cuid2";
 
-const app = new Hono().get("/", (c) => c.json({ data: "list books" }));
+import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
+import { zValidator } from "@hono/zod-validator";
+
+import { db } from "@/db/drizzle";
+import { accounts, insertAccountSchema } from "@/db/schema";
+import { eq } from "drizzle-orm";
+
+const app = new Hono()
+  .get("/", clerkMiddleware(), async (c) => {
+    const auth = getAuth(c);
+    if (!auth?.userId) return c.json({ error: "Unauthorized" }, 401);
+
+    const data = await db
+      .select()
+      .from(accounts)
+      .where(eq(accounts.userId, auth.userId));
+
+    return c.json({ data });
+  })
+  .post(
+    "/create",
+    clerkMiddleware(),
+    zValidator(
+      "json",
+      insertAccountSchema.pick({
+        name: true,
+      })
+    ),
+    async (c) => {
+      const auth = getAuth(c);
+      if (!auth?.userId) return c.json({ error: "Unauthorized" }, 401);
+
+      const { name } = c.req.valid("json");
+
+      const [data] = await db
+        .insert(accounts)
+        .values({
+          id: createId(),
+          userId: auth.userId,
+          name,
+        })
+        .returning();
+
+      return c.json({ data });
+    }
+  );
 
 export default app;
